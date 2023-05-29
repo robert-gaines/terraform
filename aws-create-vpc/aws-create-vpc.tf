de0a95e88c4947e0ed2d674aeafd00e2cfcc6758
Robert W. Gaines <robert.gaines@internal.subterfuge.biz>
2023-05-29 11:05:17 -0700

    Routine

---
+++ aws-create-vpc/aws-create-vpc.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = ""
+}
+
+resource "aws_vpc" "camas-sec-vpc" {
+    cidr_block = ""
+    tags = {
+        Name = ""
+    }
+}
+
+/*
+output "vpc_id" {
+    value = "${aws_vpc.'vpc-name'.id}"
+}
+*/
+
